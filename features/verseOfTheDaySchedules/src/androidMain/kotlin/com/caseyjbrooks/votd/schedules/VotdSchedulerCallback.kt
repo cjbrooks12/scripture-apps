21a24aa332168332e6a2189d0b246d5e70a292cf
Casey Brooks <cjbrooks12@gmail.com>
2024-05-16 18:02:16 -0500

    Set workmanager jobs as expedited

--- features/verseOfTheDaySchedules/src/androidMain/kotlin/com/caseyjbrooks/votd/schedules/VotdSchedulerCallback.kt
+++ features/verseOfTheDaySchedules/src/androidMain/kotlin/com/caseyjbrooks/votd/schedules/VotdSchedulerCallback.kt
@@ -1,5 +1,7 @@
 package com.caseyjbrooks.votd.schedules
 
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OutOfQuotaPolicy
 import com.caseyjbrooks.di.GlobalKoinApplication
 import com.copperleaf.ballast.scheduler.workmanager.SchedulerCallback
 
@@ -8,5 +10,10 @@ internal class VotdSchedulerCallback : SchedulerCallback<VotdSchedulesContract.I
     override suspend fun dispatchInput(input: VotdSchedulesContract.Inputs) {
         val vm: VotdSchedulesViewModel = GlobalKoinApplication.get()
         vm.sendAndAwaitCompletion(input)
+    }
+
+    override fun configureWorkRequest(builder: OneTimeWorkRequest.Builder): OneTimeWorkRequest.Builder {
+        return builder
+            .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
     }
 }
