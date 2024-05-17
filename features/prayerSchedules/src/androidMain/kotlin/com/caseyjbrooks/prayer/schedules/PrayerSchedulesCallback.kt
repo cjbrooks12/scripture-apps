6e3dc8b96d20003b1d3bc4ded2fa31c75a09d7cb
Casey Brooks <cjbrooks12@gmail.com>
2024-05-17 12:20:54 -0500

    Remove expedited

--- features/prayerSchedules/src/androidMain/kotlin/com/caseyjbrooks/prayer/schedules/PrayerSchedulesCallback.kt
+++ features/prayerSchedules/src/androidMain/kotlin/com/caseyjbrooks/prayer/schedules/PrayerSchedulesCallback.kt
@@ -1,7 +1,5 @@
 package com.caseyjbrooks.prayer.schedules
 
-import androidx.work.OneTimeWorkRequest
-import androidx.work.OutOfQuotaPolicy
 import com.caseyjbrooks.di.GlobalKoinApplication
 import com.copperleaf.ballast.scheduler.workmanager.SchedulerCallback
 
@@ -10,10 +8,5 @@ internal class PrayerSchedulesCallback : SchedulerCallback<PrayerSchedulesContra
     override suspend fun dispatchInput(input: PrayerSchedulesContract.Inputs) {
         val vm: PrayerSchedulesViewModel = GlobalKoinApplication.get()
         vm.sendAndAwaitCompletion(input)
-    }
-
-    override fun configureWorkRequest(builder: OneTimeWorkRequest.Builder): OneTimeWorkRequest.Builder {
-        return builder
-            .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
     }
 }
--- features/verseOfTheDaySchedules/src/androidMain/kotlin/com/caseyjbrooks/votd/schedules/VotdSchedulerCallback.kt
+++ features/verseOfTheDaySchedules/src/androidMain/kotlin/com/caseyjbrooks/votd/schedules/VotdSchedulerCallback.kt
@@ -1,7 +1,5 @@
 package com.caseyjbrooks.votd.schedules
 
-import androidx.work.OneTimeWorkRequest
-import androidx.work.OutOfQuotaPolicy
 import com.caseyjbrooks.di.GlobalKoinApplication
 import com.copperleaf.ballast.scheduler.workmanager.SchedulerCallback
 
@@ -10,10 +8,5 @@ internal class VotdSchedulerCallback : SchedulerCallback<VotdSchedulesContract.I
     override suspend fun dispatchInput(input: VotdSchedulesContract.Inputs) {
         val vm: VotdSchedulesViewModel = GlobalKoinApplication.get()
         vm.sendAndAwaitCompletion(input)
-    }
-
-    override fun configureWorkRequest(builder: OneTimeWorkRequest.Builder): OneTimeWorkRequest.Builder {
-        return builder
-            .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
     }
 }
